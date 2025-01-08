b960bc9241ff10931ee9bcef47dea101948dc57b
Sandom <sandomhet@gmail.com>
2025-01-07 19:47:57 -0800

    First Commit

---
+++ src/test/java/com/sandom/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandom.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
